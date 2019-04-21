c19bf5f2bb3d1ff5938bba0d301c854d02e495ca
Murugan R <muruga4046@gmail.com>
2019-04-21 20:51:16 +0530

    This is a new commit for what I originally planned to be amended

---
+++ Adminportal/app.js
@@ -0,0 +1,87 @@
+/**
+* Module dependencies.
+*/
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+//var methodOverride = require('method-override');
+var session = require('express-session');
+var app = express();
+var mysql      = require('mysql');
+var bodyParser=require("body-parser");
+
+
+
+var connection_  = require('express-myconnection'); 
+
+
+app.use(
+    
+    connection_(mysql,{
+        
+        host: '139.59.57.126', //'localhost',
+        user: 'root',
+        password : 'admin123',
+        port : 3306, //port mysql
+        database:'molc_test'
+
+    },'request') //or single
+
+);
+
+var customers = require('./routes/customers'); 
+var products = require('./routes/products');
+
+app.get('/products',products.list);
+
+
+
+
+//app.get('/', routes.index);
+app.get('/customers', customers.list);
+//app.get('/customers/add', customers.add);
+app.post('/customers/add', customers.save);
+app.get('/customers/delete/:id', customers.delete_customer);
+app.get('/customers/edit/:id', customers.edit);
+app.post('/customers/edit/:id',customers.save_edit);
+var connection = mysql.createConnection({
+              host     : '139.59.57.126',
+              user     : 'root',
+              password : 'admin123',
+              database : 'molc_test'
+            });
+ 
+connection.connect();
+ 
+global.db = connection;
+ 
+// all environments
+app.set('port', process.env.PORT || 8080);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+              secret: 'keyboard cat',
+              resave: false,
+              saveUninitialized: true,
+              cookie: { maxAge: 60000 }
+            }))
+ 
+// development only
+ 
+app.get('/', routes.index);//call for main index page
+app.get('/signup', user.signup);//call for signup page
+app.post('/signup', user.signup);//call for signup post 
+app.get('/login', routes.index);//call for login page
+app.post('/login', user.login);//call for login post
+app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
+app.get('/home/logout', user.logout);//call for logout
+app.get('/home/profile',user.profile);//to render users profile
+//Middleware
+app.listen(8080)
---
+++ Adminportal/routes/customers.js
@@ -0,0 +1,134 @@
+
+/*
+ * GET users listing.
+ */
+
+exports.list = function(req, res){
+
+  req.getConnection(function(err,connection){
+       
+        var query = connection.query('SELECT * FROM customers',function(err,rows)
+        {
+            
+            if(err)
+                console.log("Error Selecting : %s ",err );
+            //console.log(rows);
+            //res.render('customers',{page_title:"Customers - Node.js",data:rows});
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(rows));
+			//console.log(JSON.stringify(rows));
+            //res.end(JSON.stringify(rows));  
+           
+         });
+         
+         //console.log(query.sql);
+    });
+  
+};
+
+exports.edit = function(req, res){
+    
+    var id = req.params.id;
+    
+    req.getConnection(function(err,connection){
+       
+        var query = connection.query('SELECT * FROM customers WHERE CustomerID = ?',[id],function(err,rows)
+        {
+            
+            if(err)
+                console.log("Error Selecting : %s ",err );
+     
+           // res.render('edit_customer',{page_title:"Edit Customers - Node.js",data:rows});
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(rows));   
+           
+         });
+         
+         //console.log(query.sql);
+    }); 
+};
+
+/*Save the customer*/
+exports.save = function(req,res){
+    
+    var input = JSON.parse(JSON.stringify(req.body));
+    
+    req.getConnection(function (err, connection) {
+        
+        var data = {
+            
+            FirstName    : input.FirstName,
+            City : input.City,
+            EmailID   : input.EmailID,
+            ContactNo   : input.ContactNo 
+        
+        };
+       console.log('save request...', data);
+
+        var query = connection.query("INSERT INTO customers set ? ",data, function(err, rows)
+        {
+  
+          if (err)
+              console.log("Error inserting : %s ",err );
+         
+          //res.redirect('/customers');
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify('success'));
+          
+        });
+        
+       // console.log(query.sql); get raw query
+    
+    });
+};
+
+exports.save_edit = function(req,res){
+    
+    var input = JSON.parse(JSON.stringify(req.body));
+    var id = req.params.id;
+    
+    req.getConnection(function (err, connection) {
+        
+        var data = {
+            
+            FirstName    : input.FirstName,
+            City : input.City,
+            EmailID   : input.EmailID,
+            ContactNo   : input.ContactNo 
+        
+        };
+        
+        connection.query("UPDATE customers set ? WHERE CustomerID = ? ",[data,id], function(err, rows)
+        {
+  
+          if (err)
+              console.log("Error Updating : %s ",err );
+         
+          res.redirect('/customers');
+          
+        });
+    
+    });
+};
+
+
+exports.delete_customer = function(req,res){
+          
+     var id = req.params.id;
+    
+     req.getConnection(function (err, connection) {
+        
+        connection.query("DELETE FROM customers  WHERE CustomerID = ? ",[id], function(err, rows)
+        {
+            
+             if(err)
+                 console.log("Error deleting : %s ",err );
+            
+             res.redirect('/customers');
+             
+        });
+        
+     });
+};
+
+
